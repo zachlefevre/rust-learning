64033d1a3375ee10fd70456e9cef97962ef44045
Zlef <me@zacharylefevre.com>
2024-07-02 15:40:10 -0500

    rewrite using copy

--- src/bin/echo-server-copy.rs
+++ src/bin/echo-server-copy.rs
@@ -1,15 +1,16 @@
 use std::error::Error;
 
-use tokio::{net::TcpListener, io::{self}};
+use tokio::{net::TcpListener, io};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("127.0.0.1:8888").await?;
+
     loop {
         let (socket, _) = listener.accept().await?;
         tokio::spawn(async {
-            let (mut rd, mut wt) = io::split(socket);
-            io::copy(&mut rd, &mut wt).await
+            let (mut reader, mut writer) = io::split(socket);
+            io::copy(&mut reader, &mut writer).await.unwrap();
         });
     }
 }
