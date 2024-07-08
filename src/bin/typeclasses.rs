e2b4b0c3003e6763777ba5dc28ed377b339ced83
Zlef <me@zacharylefevre.com>
2024-07-07 19:50:51 -0500

    typeclasses

--- src/bin/typeclasses.rs
+++ src/bin/typeclasses.rs
@@ -1,17 +1,20 @@
 // This requires mount_point to work over FileDownloadRequests over Bootp, even though that is *nonsense*
 mod bad {
+    // these are protocol handlers
     pub mod nfs {
         pub struct AuthInfo {
         }
     }
 
+    // these are protocol handlers
     pub mod bootp {
         pub struct AuthInfo {
         }
     }
 
+    // this unifies the two protocol handlers to form the protocol interface
     enum AuthInfo {
-        Nfs{auth_info: crate::bad::nfs::AuthInfo, mount_point: String}, Bootp{auth_info: crate::bad::bootp::AuthInfo}
+        Nfs{mount_point: String, auth_info: nfs::AuthInfo}, Bootp{auth_info: bootp::AuthInfo}
     }
 
     struct FileDownloadRequest {
@@ -29,6 +32,72 @@ mod bad {
     }
 }
 
+mod good {
+    pub mod nfs {
+        #[derive(Clone)]
+        pub struct AuthInfo(String);
+    }
+
+    pub mod bootp {
+        pub struct AuthInfo();
+    }
+
+    pub mod protocol {
+        use super::{nfs, bootp};
+
+        pub trait ProtocolKind {
+            type AuthInfo;
+            fn auth_info(&self) -> Self::AuthInfo;
+        }
+
+        pub struct Nfs {
+            auth: nfs::AuthInfo,
+            pub mount_point: String
+        }
+
+        impl ProtocolKind for Nfs {
+            type AuthInfo = nfs::AuthInfo;
+
+            fn auth_info(&self) -> Self::AuthInfo {
+                self.auth.clone()
+            }
+        }
+
+        pub struct Bootp;
+
+        impl ProtocolKind for Bootp {
+            type AuthInfo = bootp::AuthInfo;
+
+            fn auth_info(&self) -> Self::AuthInfo {
+                bootp::AuthInfo()
+            }
+        }
+
+    }
+
+    pub struct FileDownloadRequest<P: protocol::ProtocolKind> {
+        file_name: String, protocol: P
+    }
+
+    impl<P: protocol::ProtocolKind> FileDownloadRequest<P> {
+        pub fn file_name(&self) -> &String {
+            &self.file_name
+        }
+    }
+
+    impl FileDownloadRequest<protocol::Nfs> {
+        pub fn mount_point(&self) -> &String {
+            &self.protocol.mount_point
+        }
+    }
+}
+
 fn main() {
-    
+    let req: crate::good::FileDownloadRequest<crate::good::protocol::Nfs> = todo!();
+    req.file_name();
+    req.mount_point();
+    let req: crate::good::FileDownloadRequest<crate::good::protocol::Bootp> = todo!();
+    req.file_name();
+    //req.mount_point();
+
 }
