d5797f11698b3fd8d5c14f2e5ae09458782111c6
Zlef <me@zacharylefevre.com>
2024-07-08 23:29:49 -0500

    construct our own handler struct

--- src/bin/service-trait.rs
+++ src/bin/service-trait.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, future::Future};
+use std::{error::Error, future::Future, pin::Pin};
 
 use tokio::{net::TcpListener, io::{self, AsyncRead, AsyncWrite}};
 
@@ -26,13 +26,15 @@ fn write_error(_error: Box<dyn Error>, _write: impl AsyncWrite) {
 
 
 trait Handler {
-    async fn call(&mut self, req: HttpRequest) -> HttpResponse;
+    // we let the user choose their output type, although it *must* be a Future of this particuar shape
+    type Future: Future<Output = Result<HttpResponse, Box<dyn Error>>>;
+
+    fn call(&mut self, req: HttpRequest) -> Self::Future;
 }
 
 impl Server {
-    async fn run<F, Fut>(self, handler: F) -> Result<(), Box<dyn Error>> where
-        F: Fn(HttpRequest) -> Fut,
-        Fut: Future<Output = Result<HttpResponse, Box<dyn Error>>>
+    async fn run<F>(self, mut handler: F) -> Result<(), Box<dyn Error>> where
+        F: Handler
     {
         let listener = TcpListener::bind(self.addr).await?;
 
@@ -40,7 +42,7 @@ impl Server {
             let (socket, _) = listener.accept().await?;
             let (read, write) = io::split(socket);
             let request = make_request(read);
-            let response = handler(request).await;
+            let response = handler.call(request).await;
             match response {
                 Ok(http_response) => write_response(http_response, write), // the compiler is smart enough to see write is used exclusively in these two arms and therefore is not moved in the *other* arm.
                 Err(err) => write_error(err, write),
@@ -50,19 +52,30 @@ impl Server {
 }
 
 
+struct HttpHandler;
+impl Handler for HttpHandler {
+    type Future = Pin<Box<dyn Future<Output = Result<HttpResponse, Box<dyn Error>>>>>;
+
+    fn call(&mut self, req: HttpRequest) -> Self::Future {
+        Box::pin(async move
+                 {
+                     if req.path == "/" {
+                         Ok(HttpResponse)
+                     } else {
+                         Ok(HttpResponse)
+                     }
+                 }
+        )
+    }
+}
+
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let server = Server {
         addr: "127.0.0.1:8888".into()
     };
 
-    async fn handler(req: HttpRequest) -> Result<HttpResponse, Box<dyn Error>> {
-        if req.path == "/" {
-            Ok(HttpResponse)
-        } else {
-            Ok(HttpResponse)
-        }
-    }
-
-    server.run(handler).await
+    server.run(HttpHandler).await
 }
