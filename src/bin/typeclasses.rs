9b7deadb3c8877110944eb1a31b0dde3d98a00f8
Zlef <me@zacharylefevre.com>
2024-07-14 15:20:13 -0500

    complect a bit

--- src/bin/typeclasses.rs
+++ src/bin/typeclasses.rs
@@ -3,20 +3,27 @@ mod example {
 
     mod nfs {
         #[derive(Clone)]
-        pub struct AuthInfo(String);
+        pub struct AuthInfo(pub String);
     }
 
     mod bootp {
-        pub struct AuthInfo(i32);
+        pub struct AuthInfo(pub i32);
     }
 
     mod protocol {
-        use super::nfs::{self, AuthInfo};
+        use super::{nfs::{self, AuthInfo}, bootp};
 
+
+        pub struct TransferParameters {
+            chunk_size: u32,
+            bitrate: u32,
+        }
 
         pub trait Protocol {
             type AuthInfo;
             fn auth_info(&self) -> Self::AuthInfo;
+
+            fn transfer_parameters(&self) -> TransferParameters;
         }
 
         pub struct Nfs {
@@ -30,10 +37,24 @@ mod example {
             fn auth_info(&self) -> Self::AuthInfo {
                 self.auth.clone()
             }
+
+            fn transfer_parameters(&self) -> TransferParameters {
+                todo!()
+            }
         }
-        struct Bootp;
+        pub struct Bootp;
 
+        impl Protocol for Bootp {
+            type AuthInfo = bootp::AuthInfo;
 
+            fn auth_info(&self) -> Self::AuthInfo {
+                bootp::AuthInfo(18)
+            }
+
+            fn transfer_parameters(&self) -> TransferParameters {
+                todo!()
+            }
+        }
     }
 
 
@@ -44,7 +65,17 @@ mod example {
     impl FileDownloadRequest<protocol::Nfs> {
         fn mount_point(&self) -> &String {
             let mp = &self.protocol.mount_point;
+            let ai = self.protocol.auth_info();
+            let s = ai.0;
             mp
+        }
+    }
+
+    impl FileDownloadRequest<protocol::Bootp> {
+        fn other(&self) -> i32 {
+            let ai = self.protocol.auth_info();
+            let s = ai.0;
+            s
         }
     }
 }
