4fa18230a36424ef185f5a38bd60eeef0847ca07
Zlef <me@zacharylefevre.com>
2024-07-14 15:12:07 -0500

    reimplement typeclass example

--- src/bin/typeclasses.rs
+++ src/bin/typeclasses.rs
@@ -1,103 +1,56 @@
-// This requires mount_point to work over FileDownloadRequests over Bootp, even though that is *nonsense*
-mod bad {
-    // these are protocol handlers
-    pub mod nfs {
-        pub struct AuthInfo {
-        }
-    }
+mod example {
+    use self::protocol::Protocol;
 
-    // these are protocol handlers
-    pub mod bootp {
-        pub struct AuthInfo {
-        }
-    }
-
-    // this unifies the two protocol handlers to form the protocol interface
-    enum AuthInfo {
-        Nfs{mount_point: String, auth_info: nfs::AuthInfo}, Bootp{auth_info: bootp::AuthInfo}
-    }
-
-    struct FileDownloadRequest {
-        file_name: String,
-        auth_info: AuthInfo
-    }
-
-    impl FileDownloadRequest {
-        fn mount_point(&self) -> Option<&String> {
-            match &self.auth_info {
-                AuthInfo::Nfs{mount_point, ..} => Some(mount_point),
-                AuthInfo::Bootp{..} => None,
-            }
-        }
-    }
-}
-
-mod good {
-    pub mod nfs {
+    mod nfs {
         #[derive(Clone)]
         pub struct AuthInfo(String);
     }
 
-    pub mod bootp {
-        pub struct AuthInfo();
+    mod bootp {
+        pub struct AuthInfo(i32);
     }
 
-    pub mod protocol {
-        use super::{nfs, bootp};
+    mod protocol {
+        use super::nfs::{self, AuthInfo};
 
-        pub trait ProtocolKind {
+
+        pub trait Protocol {
             type AuthInfo;
             fn auth_info(&self) -> Self::AuthInfo;
         }
 
         pub struct Nfs {
-            auth: nfs::AuthInfo,
-            pub mount_point: String
+            auth: AuthInfo,
+            pub mount_point: String,
         }
 
-        impl ProtocolKind for Nfs {
+        impl Protocol for Nfs {
             type AuthInfo = nfs::AuthInfo;
 
             fn auth_info(&self) -> Self::AuthInfo {
                 self.auth.clone()
             }
         }
+        struct Bootp;
 
-        pub struct Bootp;
-
-        impl ProtocolKind for Bootp {
-            type AuthInfo = bootp::AuthInfo;
-
-            fn auth_info(&self) -> Self::AuthInfo {
-                bootp::AuthInfo()
-            }
-        }
 
     }
 
-    pub struct FileDownloadRequest<P: protocol::ProtocolKind> {
+
+    struct FileDownloadRequest<P: protocol::Protocol> {
         file_name: String, protocol: P
     }
 
-    impl<P: protocol::ProtocolKind> FileDownloadRequest<P> {
-        pub fn file_name(&self) -> &String {
-            &self.file_name
-        }
-    }
-
     impl FileDownloadRequest<protocol::Nfs> {
-        pub fn mount_point(&self) -> &String {
-            &self.protocol.mount_point
+        fn mount_point(&self) -> &String {
+            let mp = &self.protocol.mount_point;
+            mp
         }
     }
 }
 
-fn main() {
-    let req: crate::good::FileDownloadRequest<crate::good::protocol::Nfs> = todo!();
-    req.file_name();
-    req.mount_point();
-    let req: crate::good::FileDownloadRequest<crate::good::protocol::Bootp> = todo!();
-    req.file_name();
-    //req.mount_point();
 
+
+fn main() {
+    
 }
