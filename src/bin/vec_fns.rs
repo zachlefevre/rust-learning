594465a2e646bc365d606c097628ac3cff57408f
Zlef <me@zacharylefevre.com>
2024-07-16 15:17:32 -0500

    experiment with buf reader

--- src/bin/vec_fns.rs
+++ src/bin/vec_fns.rs
@@ -1,7 +1,17 @@
+use std::io::{BufReader, self, BufRead};
+
 fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    v.push(22);
-    dbg!(v.capacity());
-    v.resize(10, 0);
-    dbg!(v.capacity());
+    let mut file = std::fs::File::open("vec_fns.rs").unwrap();
+
+    let mut bufreader = BufReader::new(file);
+
+
+    let mut s = String::new();
+    let mut v = Vec::new();
+
+    bufreader.read_until(b'B', &mut v);
+
+//    let _ = io::copy(&mut file, &mut v);
+    dbg!(String::from_utf8(v));
+
 }
