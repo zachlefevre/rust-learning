3616b5512e01afcefb1d5e68afa2527ba0c36e7f
Zlef <me@zacharylefevre.com>
2024-07-16 15:40:11 -0500

    play with bufreaders

--- src/bin/vec_fns.rs
+++ src/bin/vec_fns.rs
@@ -1,17 +1,21 @@
-use std::io::{BufReader, self, BufRead};
+use std::io::{BufReader, self, BufRead, Write, Read};
 
 fn main() {
     let mut file = std::fs::File::open("vec_fns.rs").unwrap();
 
-    let mut bufreader = BufReader::new(file);
+    let mut bufreader = BufReader::new(file).take(100);
 
 
     let mut s = String::new();
-    let mut v = Vec::new();
+ 
+
+   let mut v = Vec::new();
 
     bufreader.read_until(b'B', &mut v);
 
 //    let _ = io::copy(&mut file, &mut v);
     dbg!(String::from_utf8(v));
+
+    io::copy(&mut bufreader, &mut io::stderr());
 
 }
