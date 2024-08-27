f4e093d27ef593f610e566b97a38fa0769b22955
Zlef <me@zacharylefevre.com>
2024-08-26 19:56:51 -0500

    produce function with type bound with associated type

--- src/bin/iterators.rs
+++ src/bin/iterators.rs
@@ -1,3 +1,16 @@
+fn t<T: Iterator<Item=f32>>(t: T) -> f32 {
+    t.take(10).map(|item| item.sin()).sum()
+}
+
+struct Foo;
+impl Iterator for Foo {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(1.1)
+    }
+}
+
 fn main() {
     let items = ["hey", "there", "person"].map(String::from);
     for (k, v) in items.iter().enumerate() {
@@ -7,4 +20,5 @@ fn main() {
     for (k, v) in items.iter().zip(items.iter()) {
         dbg!(k, v);
     }
+    dbg!(t(Foo));
 }
