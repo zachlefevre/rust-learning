c16798a2f659f9f7b7802cc1e8410467e7f15a9a
Zlef <me@zacharylefevre.com>
2024-09-09 09:32:54 -0500

    play with lifetimes

--- src/bin/lifetimes.rs
+++ src/bin/lifetimes.rs
@@ -9,6 +9,10 @@ struct Person2<'a> {
     name: &'a str
 }
 
+fn do_thing(i: &mut i32) {
+    *i += 18
+}
+
 fn main() {
     let array = "hey there person".split(" ").map(String::from).collect::<Vec<_>>();
     let m = &array[0]; // just borrow out of the vector of strings
@@ -26,5 +30,19 @@ fn main() {
 
     let v = "hey there person".split(" ").map(String::from).collect::<Vec<_>>();
     dbg!(&v[0]);
+
+
+    let mut t = 12;
+    do_thing(&mut t);
+    dbg!(t);
+
+
+    let op = Some("hey there person".to_string());
+
+    if let Some(ref thing) = op {
+        dbg!(thing);
+    }
+
+    dbg!(op);
 
 }
